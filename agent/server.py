c79fcc99acb77a8144b5ac93947230aa979f38da
chaulaode <denguyenst@gmail.com>
2025-01-02 11:30:53 +0700

    Update server.py

--- agent/server.py
+++ agent/server.py
@@ -40,7 +40,7 @@ class Server(Base):
         url = registry["url"]
         # username = registry["username"]
         # password = registry["password"]
-        return sefl.execute(f"gcloud auth configure-docker")
+        return self.execute(f"gcloud auth configure-docker")
         #return self.execute(f"docker login {url}")
 
     @step("Initialize Bench")
