cf6fe88514032babc678e1f515bcf4bd1391b0b3
chaulaode <denguyenst@gmail.com>
2025-04-29 11:45:51 +0700

    Update bench.py

--- agent/bench.py
+++ agent/bench.py
@@ -172,8 +172,8 @@ class Bench(Base):
         interactive = "-i" if input else ""
         workdir = "/home/frappe/frappe-bench"
         interactive = "-i" if input else ""
-        command_chown = f"sudo docker exec -u root -w {workdir} {interactive} {self.name} chown -R frappe:frappe ."
-        self.execute(command_chown)
+        # command_chown = f"sudo docker exec -u root -w {workdir} {interactive} {self.name} chown -R frappe:frappe ."
+        # self.execute(command_chown)
         if subdir:
             workdir = os.path.join(workdir, subdir)
 
@@ -185,8 +185,8 @@ class Bench(Base):
                 "output"
             ].split()[0]
             command = f"sudo docker exec -w {workdir} " f"{interactive} {service}.1.{task} {command}"
-            command_chown = f"sudo docker exec -u root -w {workdir} " f"{interactive} {service}.1.{task} chown -R frappe:frappe ."
-            self.execute(command_chown)
+            # command_chown = f"sudo docker exec -u root -w {workdir} " f"{interactive} {service}.1.{task} chown -R frappe:frappe ."
+            # self.execute(command_chown)
         
         return self.execute(command, input=input, non_zero_throw=non_zero_throw)
 
